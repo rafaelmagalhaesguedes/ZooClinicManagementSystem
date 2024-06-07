f522dfa336df8a0b27f7953d5f97e5cda4ccc512
rafael <rafaelmagalhaes1@live.com>
2024-06-07 15:18:36 -0300

    initial commit

---
+++ src/main/java/com/zooclinic/ZoonosesApplication.java
@@ -0,0 +1,13 @@
+package com.zooclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoonosesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoonosesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zooclinic/ZoonosesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoonosesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
